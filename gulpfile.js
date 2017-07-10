abca20f4cf5347c3d883165a517b88f0989b33f5
Chris DeMars <cdemars86@gmail.com>
2017-07-10 14:47:18 -0400

    new images

--- gulpfile.js
+++ gulpfile.js
@@ -20,6 +20,7 @@ gulp.task('serve', ['sass'], function() {
 
     gulp.watch('src/scss/*.scss', ['sass']);
     gulp.watch('src/js/*.js', ['js']);
+    gulp.watch('src/img/**/*.+(png|jpg|gif|svg)')
     gulp.watch('./*.html').on('change', browserSync.reload);
 });
 
@@ -54,6 +55,7 @@ gulp.task('images', function () {
 gulp.task('watch', function () {
   gulp.watch('src/scss/*.scss', ['sass']);
   gulp.watch('src/js/*.js', ['js']);
+  gulp.watch('src/img/**/*.+(png|jpg|gif|svg)')
   gulp.watch('./*.html').on('change', browserSync.reload);
 });
 
